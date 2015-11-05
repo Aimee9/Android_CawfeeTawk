8ecd572096beec337d0ff535c84bd081862fafcb
Aimee <akmreiss@yahoo.com>
2015-11-05 10:29:40 -0800

    Create a new Activity

---
+++ app/src/main/java/com/example/guest/cawfeetawk/model/Message.java
@@ -0,0 +1,7 @@
+package com.example.guest.cawfeetawk.model;
+
+/**
+ * Created by Guest on 11/5/15.
+ */
+public class Message {
+}
---
+++ app/src/main/java/com/example/guest/cawfeetawk/ui/MessagesActivity.java
@@ -0,0 +1,20 @@
+package com.example.guest.cawfeetawk.ui;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.guest.cawfeetawk.R;
+
+public class MessagesActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages);
+    }
+
+
+}
