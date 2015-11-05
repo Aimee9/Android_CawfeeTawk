0e73a1c020e6255ba759bb69038291c08250ba19
Aimee <akmreiss@yahoo.com>
2015-11-05 10:30:53 -0800

    Create a model Message for what the users will write

--- app/src/main/java/com/example/guest/cawfeetawk/model/Message.java
+++ app/src/main/java/com/example/guest/cawfeetawk/model/Message.java
@@ -1,7 +1,40 @@
 package com.example.guest.cawfeetawk.model;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+
+import java.util.List;
+
 /**
  * Created by Guest on 11/5/15.
  */
-public class Message {
+@Table(name = "Messages", id = "_id")
+public class Message extends Model {
+
+    @Column(name = "Content")
+    private String mContent;
+
+    public Message() {
+        super();
+    }
+
+    public Message(String content) {
+        mContent = content;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        mContent = content;
+    }
+
+    public static List<Message> all(){
+        return new Select()
+                .from(Message.class)
+                .execute();
+    }
 }
